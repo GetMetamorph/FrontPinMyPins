30d51ce9035d09ededdac15edc0eb71d7bdfc8ec
Sabbagh <anthonysabbagh03@gmail.com>
2022-03-20 18:58:37 +0100

    feat: connect events

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const ejs = require('ejs');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('pages/index');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 server.listen(3000, () => {
