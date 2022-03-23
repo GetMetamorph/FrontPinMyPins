e2d65a00d7785498ab515984b6868869396b7146
Myhed <benrhouma.myhed@gmail.com>
2022-03-23 23:57:40 +0100

    add cors origin acces-control socket

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {cors: {origin: "*"}});
 
 app.set('view engine', 'ejs');
 
